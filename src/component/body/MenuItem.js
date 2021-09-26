712c598d040acaea088c0bf2e4de442715cb5362
Ariful Islam <a.islamdiu@gmail.com>
2021-09-26 20:40:58 +0600

    menuItem dedign

--- src/component/body/MenuItem.js
+++ src/component/body/MenuItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, CardTitle, CardBody, CardImg, CardImgOverlay } from "reactstrap";
 
 const MenuItem = props => {
 
@@ -6,7 +7,14 @@ const MenuItem = props => {
 
     return(
         <div>
-
+            <Card style={{margin:"10px"}}>
+                <CardBody>
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{opacity:"0.6"}} />
+                    <CardImgOverlay>
+                        <CardTitle>{props.dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     )
 }
